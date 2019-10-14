3e4fc13c7dfd00e7c270623e123ebeb826d28d39
phanx211 <phanx211@umn.edu>
2019-10-14 00:05:43 -0500

    Initial commmit, project progress

---
+++ src/com/company/FoodItem.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class FoodItem {
+    private int calories;
+    private String foodGroup;
+    private String name;
+
+    // Default Constructor
+    public FoodItem() {
+        this.calories = 0;
+        this.foodGroup = "";
+        this.name = "";
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        System.out.println("Hi! Welcome to the calorie calculator. \nI will calculate the amount of calories in your food" +
+                " based on the weight you enter.\n" +
+                "");
+    }
+}
